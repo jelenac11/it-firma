be3ab4cbdbf84adce78d55f616d1688420a5ba8b
Milan Marinkovic <milan_marinkovic98@hotmail.com>
2021-12-04 12:47:44 +0100

    Create base architecture for PSP

---
+++ psp/eureka/src/test/java/tim13/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package tim13.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ psp/paypal/src/main/java/tim13/paypal/PaypalServiceApplication.java
@@ -0,0 +1,15 @@
+package tim13.paypal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaypalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypalServiceApplication.class, args);
+	}
+
+}
